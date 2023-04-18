bfc2991f8facc82eb712c073e3a5a680607e33e0
ivica <blizujemars@gmail.com>
2023-04-18 15:09:00 +0200

    refactor

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import requests
 from collections import deque
 import time
-
-# subject = 'Python (programming language)'
-# target= 'Human'
+from requests.exceptions import HTTPError
 
 
- 
-# response = requests.get(url, params=params)
-# data = response.json()
- 
-# queue = deque([(subject, [subject])])
-# page, path = queue.popleft()
 
-# for link in data['parse']['links']:
-#     if not link['ns']:
-#         queue.append((link['*'], path + [link['*']]))
 
 url = 'https://en.wikipedia.org/w/api.php'
 params = {
         'action': 'parse',        
         'prop': 'links',
-        'format': 'json',
-        
+        'format': 'json',        
     }
+
+rand_params = {
+    'action': 'query',
+    'format': 'json',
+    'list': 'random',    
+    'rnnamespace': 0
+}
+
+def get_page(params):
+    response = requests.get(url, params=params)
+    return response.json()        
+
 
 def bfs_wikipedia(start_page, target_page):
     print(f"finding path from {start_page} to {target_page}")
@@ -34,7 +34,7 @@ def bfs_wikipedia(start_page, target_page):
 
     while queue and not found:
         page, path = queue.popleft()
-        print(f"checking - {page}")
+        #print(f"checking - {page}")
         
         
         if page not in visited:
@@ -46,24 +46,33 @@ def bfs_wikipedia(start_page, target_page):
             else:
                 try:
                     params['page'] = page                
-                    response = requests.get(url, params=params)
-                    data = response.json()                                
-                    for link in data['parse']['links']:
+                    links = get_page(params)                                                
+                    for link in links['parse']['links']:
                         if not link['ns']:
                             if link['*'] not in visited:
                                 if link['*'] == target_page:
                                     queue.appendleft((link['*'], path + [link['*']]))   
                                 else: 
                                     queue.append((link['*'], path + [link['*']]))   
-                
-                except Exception:                
+            
+                except HTTPError as e:                
+                    print(e.response.text)
+               
+                except Exception as e:
                     pass
 
           
     return found
-            
-start = time.time()
-print(bfs_wikipedia("Fly", "Canada"))
-end = time.time()
-print(f"Time elapsed: {round(((end - start)/60), 2)}")
+
+
+
+def main():
+    start_page = get_page(rand_params)['query']['random'][0]['title']
+
+    start = time.time()
+    print(' --> '.join(bfs_wikipedia(start_page, 'Rome')))
+    end = time.time()    
+    print(f"Time (s): {round(end - start, 2)}")
+
+main()
 
