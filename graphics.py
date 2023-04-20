a083eb20efc1a369c21c754417ca315a4ecdf3df
ivica <blizujemars@gmail.com>
2023-04-20 14:40:43 +0200

    add stop_search, toggle_button_state, error_msg

--- graphics.py
+++ graphics.py
@@ -13,28 +13,45 @@ class Window:
         #self.__root.configure(bg='red')
         self.canvas = Canvas(bg='skyblue', height=1000, width=400)
         self.canvas.place(x=200, y=0)
-        self.__start_button = Button(self.__root, text='Start', command=self.__click)
-        self.__start_button.place(x=10, y=100)
-        self.__input = Entry(self.__root)
-        self.__input.place(x=10, y=50)   
-        self.__rand_button = Button(self.__root, text='Random', command=self.__get_random_page)     
-        self.__rand_button.place(x=10, y=150)
+        self.start_button = Button(self.__root, text='Start', command=self.click)
+        self.start_button.place(x=10, y=100)
+        self.stop_button = Button(self.__root, text='Stop', command=self.stop_search)
+        self.stop_button.place(x=10, y=200)
+        self.input = Entry(self.__root)
+        self.input.place(x=10, y=50)   
+        self.rand_button = Button(self.__root, text='Random Page', command=self.get_random_page)     
+        self.rand_button.place(x=10, y=150)
+        self.search = Search()        
+        self.error_message = Label(self.__root, bg='red')
         self.running = False
+        self.stop = False
 
-        self.__search = Search()
-
-    def __click(self):
-        
-        start_page = self.__input.get()
+    def stop_search(self):
+        self.stop = True
+        self.toggle_buttons_state('normal')
+  
+    def toggle_buttons_state(self, state):
+        self.start_button.config(state=state)        
+        self.rand_button.config(state=state)  
+    
+    def click(self):
+        self.stop = False
+        self.toggle_buttons_state('disabled')       
+        start_page = self.input.get()
         if start_page == '':
+            self.toggle_buttons_state('normal')
             return        
-       
-        if self.__search.page_error(start_page):
-            #TODO: Clear lables
-            Label(self.__root, text=f'Page for {start_page} does not exist. Try another one.', bg='red').pack()
         
+        if self.search.page_error(start_page):
+            self.display_error_message(f"Page {start_page} does not exist. Try another one.")
+            self.toggle_buttons_state('normal')
+            return
+        
+        self.error_message.pack_forget()         
         start_time = time.time()
-        for i in self.__search.bfs_wikipedia(start_page, 'Rome'):
+        for i in self.search.bfs_wikipedia(start_page, 'Rome'):
+            if self.stop:
+                return            
             self.canvas.delete('all')
             self.draw_nodes(i[0])
             total = Info()
@@ -43,28 +60,33 @@ class Window:
         end_time = time.time()
         info = Info()
         info.draw_time(self.canvas, round(end_time - start_time))
-        
-        # if not found:
-        #     Label(self.__root, text=f'Path not found from {start_page} to Rome', bg='red').pack()
+        self.toggle_buttons_state('normal')
+           
+      
 
-    def draw_nodes(self, i):
+    def display_error_message(self, error_message):
+        self.error_message.config(text=error_message)
+        self.error_message.pack()
+
+
+    def draw_nodes(self, path_array):
         y0 = 50
         y1 = 70
-        for j in range(len(i)):
-            if j:
+        for node in range(len(path_array)):
+            if node:
                 y0 += 80
                 y1 += 80
             arrow = True
-            if j == len(i) - 1:
+            if node == len(path_array) - 1:
                 arrow = False 
-            p = Wiki_Page(200, y0, 220, y1, i[j])
+            p = Wiki_Page(200, y0, 220, y1, path_array[node])
             p.draw(self.canvas, arrow)
       
             
-    def __get_random_page(self):
-        random_page = self.__search.get_page(random=True)['query']['random'][0]['title']
-        self.__input.delete(0, 'end')      
-        self.__input.insert(0, random_page)      
+    def get_random_page(self):
+        random_page = self.search.get_page(random=True)['query']['random'][0]['title']
+        self.input.delete(0, 'end')      
+        self.input.insert(0, random_page)      
            
     def redraw(self):
         self.__root.update_idletasks()
